ba3c2b83e17d7a40843166fb51d638b410489f33
Mayank parashar <mayankparashar1257@gmail.com>
2018-03-21 04:42:42 +0530

    program now able to to new words(from input file) to already existing dictionary,change in write_dictionary method(now using BufferedWriter),adding new folder Dictionary_version(to save all important versions)

--- src/dictionary/Dictionary_creator.java
+++ src/dictionary/Dictionary_creator.java
@@ -19,11 +19,12 @@ public class Dictionary_creator {
 		String pdf="Input.txt";//String save name of file from which we read data and add to Dictionary
 		String dictionary="Dictionary.txt";//String save name of Dictionary file
 		HashMap<String,Integer> map=new HashMap<String,Integer>();//Map use to find the frequency of words so that no word add to Dictionary twice and freq of words also save with them
+		add_dictionary_toMap(dictionary,map);
 		String text = new String(Files.readAllBytes(Paths.get("Input.txt")), StandardCharsets.UTF_8);//read whole file at once and save it in String
 		Pattern p = Pattern.compile("[a-zA-Z]+");//regex to match only word(small and capital)
 		Matcher m=p.matcher(text);//file which is match against regex is pass in constructor as parameter
 		long count=0;
-		while (m.find()) {count++;
+		while (m.find()) {
 			String str=m.group();
 			if(map.containsKey(str))//if map already contains that word
 			{
@@ -35,21 +36,61 @@ public class Dictionary_creator {
 				map.put(str,1);// add to map with frequency as 1
 			}
 		}
+		
 		FileWriter write=new FileWriter(dictionary);//object to write data, String contains Dictionary file name pass as parameter
-		write_dictionary(map,write);// method to write Map data in Dictionary
+		write_dictionary(map,dictionary);// method to write Map data in Dictionary
+		System.out.println(map.size());
 		//print(map);
 	}
 	
 	//Method write Map data in Dictionary
-	static void write_dictionary(HashMap<String,Integer> map,FileWriter write) throws IOException
+	static void write_dictionary(HashMap<String,Integer> map,String dictionary) throws IOException
 	{
-		Set<String> set=map.keySet();//set of all keys of Map
-		for(String s:set)
-		{
-			int value=map.get(s);
-			write.write(s+" ");
-			write.write(value+" ");
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(dictionary))) {
+
+			StringBuilder content =new StringBuilder();
+			Set<String> set=map.keySet();
+			for(String s:set)
+			{
+				int value=map.get(s);
+				content.append(s+" ");
+				content.append(value+" ");
+			}
+			bw.write(content.toString());
+			
+			// no need to close it.
+			//bw.close();
+
+			
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
 		}
+		
+	}
+	
+	static void add_dictionary_toMap(String dictionary,HashMap<String,Integer> map) throws IOException
+	{
+		String text = new String(Files.readAllBytes(Paths.get(dictionary)), StandardCharsets.UTF_8);
+		String[] ptr=text.split(" ");
+		int status=0;
+		String key="";
+		for(String s:ptr)
+		{
+			if(status%2==0)
+			{
+				key=s;
+			}
+			else
+			{
+				int value=Integer.parseInt(s);
+				map.put(key, value);
+			}
+			status++;
+		}
+		
 	}
 	
 	//Method to print Map
